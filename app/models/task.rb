e871431cb7b8eb0deec298c9a633f523b5de0711
Harris Hartung <hjh@andrew.cmu.edu>
2014-01-28 17:17:33 -0500

    modified task model to pass unit tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ActiveRecord::Base
   attr_accessible :active, :name, :points
+
+  has_many :chores
+
+  validates_presence_of :name
+  validates_numericality_of :points, :only_integer => true, greater_than_or_equal_to: 0
+
+  scope :alphabetical, order('name')
+  scope :active, where('active = ?', true)
   
 end
