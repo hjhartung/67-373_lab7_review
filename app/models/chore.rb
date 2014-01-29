78217c4aae36c623482977aee78b8681a248bfad
Harris Hartung <hjh@andrew.cmu.edu>
2014-01-28 23:52:04 -0500

    modified chore model to pass all chore unit tests

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,24 @@
 class Chore < ActiveRecord::Base
   attr_accessible :child_id, :completed, :due_on, :task_id
-  
+
+  belongs_to :child
+  belongs_to :task
+
+  validates_date :due_on
+
+  scope :by_task, joins(:task).order('tasks.name')
+  scope :chronological, order('due_on')
+  scope :pending, where('completed = ?', false)
+  scope :done, where('completed = ?', true)
+  scope :upcoming, where('due_on >= ?', Date.today)
+  scope :past, where('due_on < ?', Date.today)
+
+  def status
+  	if self.completed
+  		return "Completed"
+  	else
+  		return "Pending"
+  	end
+  end
+
 end
